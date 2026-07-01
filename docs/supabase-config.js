316d67d452476726f6d4d914f53c42a2c9714741
gianparedes2023 <gian.paredes.2023@gmail.com>
2026-07-01 14:37:44 -0500

    Corregir URL de Supabase

--- docs/supabase-config.js
+++ docs/supabase-config.js
@@ -1,4 +1,4 @@
 window.APP_SUPABASE = {
-  url: "https://ayyrsrcvkfpbhaexeyqc.supabase.co",
+  url: "https://feqrhmhtgpnmpwugnvsd.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5eXJzcmN2a2ZwYmhhZXhleXFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyMTg2ODEsImV4cCI6MjA5NDc5NDY4MX0.i5Ns02uy-pdLmON5TxkV8bcQ4p7K7mdnt412heAnkvc",
 };
